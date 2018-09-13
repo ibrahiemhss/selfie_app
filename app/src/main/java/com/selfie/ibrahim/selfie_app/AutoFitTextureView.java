fd02039e106a851b37997074b203f61b77a56981
ibrahiemhss <kekoreko3@gmail.com>
2018-09-13 03:40:21 +0200

    add tensorflow files

--- app/src/main/java/com/selfie/ibrahim/selfie_app/AutoFitTextureView.java
+++ app/src/main/java/com/selfie/ibrahim/selfie_app/AutoFitTextureView.java
@@ -1,17 +1,18 @@
-/* Copyright 2017 The TensorFlow Authors. All Rights Reserved.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+/*
+ * Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package com.selfie.ibrahim.selfie_app;
 
@@ -19,53 +20,54 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.TextureView;
 
-/** A {@link TextureView} that can be adjusted to a specified aspect ratio. */
+/**
+ * A {@link TextureView} that can be adjusted to a specified aspect ratio.
+ */
 public class AutoFitTextureView extends TextureView {
+  private int ratioWidth = 0;
+  private int ratioHeight = 0;
 
-  private int mRatioWidth = 0;
-  private int mRatioHeight = 0;
-
-  public AutoFitTextureView(Context context) {
+  public AutoFitTextureView(final Context context) {
     this(context, null);
   }
 
-  public AutoFitTextureView(Context context, AttributeSet attrs) {
+  public AutoFitTextureView(final Context context, final AttributeSet attrs) {
     this(context, attrs, 0);
   }
 
-  public AutoFitTextureView(Context context, AttributeSet attrs, int defStyle) {
+  public AutoFitTextureView(final Context context, final AttributeSet attrs, final int defStyle) {
     super(context, attrs, defStyle);
   }
 
   /**
    * Sets the aspect ratio for this view. The size of the view will be measured based on the ratio
-   * calculated from the parameters. Note that the actual sizes of parameters don't matter, that is,
-   * calling setAspectRatio(2, 3) and setAspectRatio(4, 6) make the same result.
+   * calculated from the parameters. Note that the actual sizes of parameters don't matter, that
+   * is, calling setAspectRatio(2, 3) and setAspectRatio(4, 6) make the same result.
    *
-   * @param width Relative horizontal size
+   * @param width  Relative horizontal size
    * @param height Relative vertical size
    */
-  public void setAspectRatio(int width, int height) {
+  public void setAspectRatio(final int width, final int height) {
     if (width < 0 || height < 0) {
       throw new IllegalArgumentException("Size cannot be negative.");
     }
-    mRatioWidth = width;
-    mRatioHeight = height;
+    ratioWidth = width;
+    ratioHeight = height;
     requestLayout();
   }
 
   @Override
-  protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+  protected void onMeasure(final int widthMeasureSpec, final int heightMeasureSpec) {
     super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-    int width = MeasureSpec.getSize(widthMeasureSpec);
-    int height = MeasureSpec.getSize(heightMeasureSpec);
-    if (0 == mRatioWidth || 0 == mRatioHeight) {
+    final int width = MeasureSpec.getSize(widthMeasureSpec);
+    final int height = MeasureSpec.getSize(heightMeasureSpec);
+    if (0 == ratioWidth || 0 == ratioHeight) {
       setMeasuredDimension(width, height);
     } else {
-      if (width < height * mRatioWidth / mRatioHeight) {
-        setMeasuredDimension(width, width * mRatioHeight / mRatioWidth);
+      if (width < height * ratioWidth / ratioHeight) {
+        setMeasuredDimension(width, width * ratioHeight / ratioWidth);
       } else {
-        setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
+        setMeasuredDimension(height * ratioWidth / ratioHeight, height);
       }
     }
   }
--- app/src/main/java/com/selfie/ibrahim/selfie_app/TensorFlowImageClassifier.java
+++ app/src/main/java/com/selfie/ibrahim/selfie_app/TensorFlowImageClassifier.java
@@ -1,137 +1,201 @@
+/* Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+==============================================================================*/
+
 package com.selfie.ibrahim.selfie_app;
 
-import android.annotation.SuppressLint;
-import android.content.res.AssetFileDescriptor;
 import android.content.res.AssetManager;
 import android.graphics.Bitmap;
+import android.os.Trace;
+import android.util.Log;
 
-import org.tensorflow.lite.Interpreter;
+import org.tensorflow.Operation;
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.ByteBuffer;
-import java.nio.ByteOrder;
-import java.nio.MappedByteBuffer;
-import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Vector;
 
-/**
- * Created by amitshekhar on 17/03/18.
- */
-
+/** A classifier specialized to label images using TensorFlow. */
 public class TensorFlowImageClassifier implements Classifier {
+  private static final String TAG = "TensorFlowImageClassifier";
 
-    private static final int MAX_RESULTS = 3;
-    private static final int BATCH_SIZE = 1;
-    private static final int PIXEL_SIZE = 3;
-    private static final float THRESHOLD = 0.1f;
+  // Only return this many results with at least this confidence.
+  private static final int MAX_RESULTS = 3;
+  private static final float THRESHOLD = 0.1f;
 
-    private Interpreter interpreter;
-    private int inputSize;
-    private List<String> labelList;
+  // Config values.
+  private String inputName;
+  private String outputName;
+  private int inputSize;
+  private int imageMean;
+  private float imageStd;
 
-    private TensorFlowImageClassifier() {
+  // Pre-allocated buffers.
+  private Vector<String> labels = new Vector<String>();
+  private int[] intValues;
+  private float[] floatValues;
+  private float[] outputs;
+  private String[] outputNames;
 
+  private boolean logStats = false;
+
+  private TensorFlowInferenceInterface inferenceInterface;
+
+  private TensorFlowImageClassifier() {}
+
+  /**
+   * Initializes a native TensorFlow session for classifying images.
+   *
+   * @param assetManager The asset manager to be used to load assets.
+   * @param modelFilename The filepath of the model GraphDef protocol buffer.
+   * @param labelFilename The filepath of label file for classes.
+   * @param inputSize The input size. A square image of inputSize x inputSize is assumed.
+   * @param imageMean The assumed mean of the image values.
+   * @param imageStd The assumed std of the image values.
+   * @param inputName The label of the image input node.
+   * @param outputName The label of the output node.
+   * @throws IOException
+   */
+  public static Classifier create(
+      AssetManager assetManager,
+      String modelFilename,
+      String labelFilename,
+      int inputSize,
+      int imageMean,
+      float imageStd,
+      String inputName,
+      String outputName) {
+    TensorFlowImageClassifier c = new TensorFlowImageClassifier();
+    c.inputName = inputName;
+    c.outputName = outputName;
+
+    // Read the label names into memory.
+    // TODO(andrewharp): make this handle non-assets.
+    String actualFilename = labelFilename.split("file:///android_asset/")[1];
+    Log.i(TAG, "Reading labels from: " + actualFilename);
+    BufferedReader br = null;
+    try {
+      br = new BufferedReader(new InputStreamReader(assetManager.open(actualFilename)));
+      String line;
+      while ((line = br.readLine()) != null) {
+        c.labels.add(line);
+      }
+      br.close();
+    } catch (IOException e) {
+      throw new RuntimeException("Problem reading label file!" , e);
     }
 
-    static Classifier create(AssetManager assetManager,
-                             String modelPath,
-                             String labelPath,
-                             int inputSize) throws IOException {
+    c.inferenceInterface = new TensorFlowInferenceInterface(assetManager, modelFilename);
 
-        TensorFlowImageClassifier classifier = new TensorFlowImageClassifier();
-        classifier.interpreter = new Interpreter(classifier.loadModelFile(assetManager, modelPath));
-        classifier.labelList = classifier.loadLabelList(assetManager, labelPath);
-        classifier.inputSize = inputSize;
+    // The shape of the output is [N, NUM_CLASSES], where N is the batch size.
+    final Operation operation = c.inferenceInterface.graphOperation(outputName);
+    final int numClasses = (int) operation.output(0).shape().size(1);
+    Log.i(TAG, "Read " + c.labels.size() + " labels, output layer size is " + numClasses);
 
-        return classifier;
+    // Ideally, inputSize could have been retrieved from the shape of the input operation.  Alas,
+    // the placeholder node for input in the graphdef typically used does not specify a shape, so it
+    // must be passed in as a parameter.
+    c.inputSize = inputSize;
+    c.imageMean = imageMean;
+    c.imageStd = imageStd;
+
+    // Pre-allocate buffers.
+    c.outputNames = new String[] {outputName};
+    c.intValues = new int[inputSize * inputSize];
+    c.floatValues = new float[inputSize * inputSize * 3];
+    c.outputs = new float[numClasses];
+
+    return c;
+  }
+
+  @Override
+  public List<Recognition> recognizeImage(final Bitmap bitmap) {
+    // Log this method so that it can be analyzed with systrace.
+    Trace.beginSection("recognizeImage");
+
+    Trace.beginSection("preprocessBitmap");
+    // Preprocess the image data from 0-255 int to normalized float based
+    // on the provided parameters.
+    bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
+    for (int i = 0; i < intValues.length; ++i) {
+      final int val = intValues[i];
+      floatValues[i * 3 + 0] = (((val >> 16) & 0xFF) - imageMean) / imageStd;
+      floatValues[i * 3 + 1] = (((val >> 8) & 0xFF) - imageMean) / imageStd;
+      floatValues[i * 3 + 2] = ((val & 0xFF) - imageMean) / imageStd;
     }
+    Trace.endSection();
 
-    @Override
-    public List<Recognition> recognizeImage(Bitmap bitmap) {
-        ByteBuffer byteBuffer = convertBitmapToByteBuffer(bitmap);
-        byte[][] result = new byte[1][labelList.size()];
-        interpreter.run(byteBuffer, result);
-        return getSortedResult(result);
+    // Copy the input data into TensorFlow.
+    Trace.beginSection("feed");
+    inferenceInterface.feed(inputName, floatValues, 1, inputSize, inputSize, 3);
+    Trace.endSection();
+
+    // Run the inference call.
+    Trace.beginSection("run");
+    inferenceInterface.run(outputNames, logStats);
+    Trace.endSection();
+
+    // Copy the output Tensor back into the output array.
+    Trace.beginSection("fetch");
+    inferenceInterface.fetch(outputName, outputs);
+    Trace.endSection();
+
+    // Find the best classifications.
+    PriorityQueue<Recognition> pq =
+        new PriorityQueue<Recognition>(
+            3,
+            new Comparator<Recognition>() {
+              @Override
+              public int compare(Recognition lhs, Recognition rhs) {
+                // Intentionally reversed to put high confidence at the head of the queue.
+                return Float.compare(rhs.getConfidence(), lhs.getConfidence());
+              }
+            });
+    for (int i = 0; i < outputs.length; ++i) {
+      if (outputs[i] > THRESHOLD) {
+        pq.add(
+            new Recognition(
+                "" + i, labels.size() > i ? labels.get(i) : "unknown", outputs[i], null));
+      }
     }
-
-    @Override
-    public void close() {
-        interpreter.close();
-        interpreter = null;
+    final ArrayList<Recognition> recognitions = new ArrayList<Recognition>();
+    int recognitionsSize = Math.min(pq.size(), MAX_RESULTS);
+    for (int i = 0; i < recognitionsSize; ++i) {
+      recognitions.add(pq.poll());
     }
+    Trace.endSection(); // "recognizeImage"
+    return recognitions;
+  }
 
-    private MappedByteBuffer loadModelFile(AssetManager assetManager, String modelPath) throws IOException {
-        AssetFileDescriptor fileDescriptor = assetManager.openFd(modelPath);
-        FileInputStream inputStream = new FileInputStream(fileDescriptor.getFileDescriptor());
-        FileChannel fileChannel = inputStream.getChannel();
-        long startOffset = fileDescriptor.getStartOffset();
-        long declaredLength = fileDescriptor.getDeclaredLength();
-        return fileChannel.map(FileChannel.MapMode.READ_ONLY, startOffset, declaredLength);
-    }
+  @Override
+  public void enableStatLogging(boolean logStats) {
+    this.logStats = logStats;
+  }
 
-    private List<String> loadLabelList(AssetManager assetManager, String labelPath) throws IOException {
-        List<String> labelList = new ArrayList<>();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(assetManager.open(labelPath)));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            labelList.add(line);
-        }
-        reader.close();
-        return labelList;
-    }
+  @Override
+  public String getStatString() {
+    return inferenceInterface.getStatString();
+  }
 
-    private ByteBuffer convertBitmapToByteBuffer(Bitmap bitmap) {
-        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(BATCH_SIZE * inputSize * inputSize * PIXEL_SIZE);
-        byteBuffer.order(ByteOrder.nativeOrder());
-        int[] intValues = new int[inputSize * inputSize];
-        bitmap.getPixels(intValues, 0, bitmap.getWidth(), 0, 0, bitmap.getWidth(), bitmap.getHeight());
-        int pixel = 0;
-        for (int i = 0; i < inputSize; ++i) {
-            for (int j = 0; j < inputSize; ++j) {
-                final int val = intValues[pixel++];
-                byteBuffer.put((byte) ((val >> 16) & 0xFF));
-                byteBuffer.put((byte) ((val >> 8) & 0xFF));
-                byteBuffer.put((byte) (val & 0xFF));
-            }
-        }
-        return byteBuffer;
-    }
-
-    @SuppressLint("DefaultLocale")
-    private List<Recognition> getSortedResult(byte[][] labelProbArray) {
-
-        PriorityQueue<Recognition> pq =
-                new PriorityQueue<>(
-                        MAX_RESULTS,
-                        new Comparator<Recognition>() {
-                            @Override
-                            public int compare(Recognition lhs, Recognition rhs) {
-                                return Float.compare(rhs.getConfidence(), lhs.getConfidence());
-                            }
-                        });
-
-        for (int i = 0; i < labelList.size(); ++i) {
-            float confidence = (labelProbArray[0][i] & 0xff) / 255.0f;
-            if (confidence > THRESHOLD) {
-                pq.add(new Recognition("" + i,
-                        labelList.size() > i ? labelList.get(i) : "unknown",
-                        confidence));
-            }
-        }
-
-        final ArrayList<Recognition> recognitions = new ArrayList<>();
-        int recognitionsSize = Math.min(pq.size(), MAX_RESULTS);
-        for (int i = 0; i < recognitionsSize; ++i) {
-            recognitions.add(pq.poll());
-        }
-
-        return recognitions;
-    }
-
+  @Override
+  public void close() {
+    inferenceInterface.close();
+  }
 }

8d3ad555021746c6192772d44b5444e0a50d8bef
ibrahiemhss <kekoreko3@gmail.com>
2018-09-25 00:36:41 +0200

    add new functions to capture image

---
+++ app/src/main/java/com/selfie/ibrahim/selfie_app/CameraActivity.java
@@ -0,0 +1,23 @@
+
+package com.selfie.ibrahim.selfie_app;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+
+/** Main {@code Activity} class for the Camera app. */
+public class CameraActivity extends AppCompatActivity {
+
+  @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_camera);
+    if (null == savedInstanceState) {
+      getSupportFragmentManager().beginTransaction()
+              .replace(R.id.container, Camera2BasicFragment.newInstance())
+              .commit();
+    }
+  }
+}
